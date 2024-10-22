fb9c387d77b0cf5187440ffb1ba2b433629cf1a5
Adrián Ramírez <adrianramirezgalera@gmail.com>
2024-10-22 17:06:44 +0200

    mañana seguimos

--- src/components/Layout/Header/Header.tsx
+++ src/components/Layout/Header/Header.tsx
@@ -1,3 +1,8 @@
 export default function Header() {
-  return <header className="col-span-3 bg-red-500">Header</header>;
+  return (
+    <header className="flex max-h-16 justify-between bg-red-500 lg:col-span-3 py-2 px-4">
+      <article className="bg-green-500 lg:w-1/2 ">Buscador</article>
+      <article>Perfil</article>
+    </header>
+  );
 }
